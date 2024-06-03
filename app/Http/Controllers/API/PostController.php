53df27d8fe5427745398cb5cee4266690d07e767
kxade <ndibragim@gmail.com>
2024-06-03 16:40:18 +0000

    add posts api

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -3,40 +3,63 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\StorePostRequest;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
 use Illuminate\Http\Request;
-
 
 class PostController extends Controller
 {
-    public function index() {
-        return 'Страница списка с постами';
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return PostResource::collection(Post::all()); 
     }
 
-    public function create() {
-        return 'Страница создания поста';
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => ['required', 'string', 'max:100'],
+            'content' => ['required', 'string', 'max:1000'],
+            'published_at' => ['nullable', 'string', 'date'],
+            'published' => ['nullable', 'boolean'],
+            'category_id' => 'nullable|exists:categories,id',
+            'user_id' => 'required|exists:users,id'
+        ]);
+
+        $post = Post::create($validated);
+
+        return new PostResource($post);
     }
 
-    public function store() {
-        return 'Сохранить новый пост';
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $post)
+    {
+        return new PostResource(Post::findOrFail($post));
     }
 
-    public function show($post) {
-        return "Страница с постом $post";
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StorePostRequest $request, Post $post)
+    {
+        $post->update($request->validated());
+
+        return new PostResource($post);
     }
 
-    public function edit($post) {
-        return "Страница редактирования поста $post";
-    }
-
-    public function update() {
-        return 'Обновление поста';
-    }
-
-    public function destroy() {
-        return 'Удаление поста';
-    }
-
-    public function like() {
-        return 'Лайк + 1';
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $post)
+    {
+        Post::findOrFail($post)->delete();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,9 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::prefix('api')->as('api.')->group(function () {
-    Route::get('posts', [PostController::class, 'index'])->name('posts.index');
-    Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
-    Route::post('posts/{post}/like', [PostController::class, 'like'])->name('posts.like');
-});
+// Route::get('posts', [PostController::class, 'index'])->name('posts.index');
+// Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+// Route::post('posts/{post}/like', [PostController::class, 'like'])->name('posts.like');
+
+
+Route::apiResources(['posts' => PostController::class]);
