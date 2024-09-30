52c2c8b56d55871b1af07115472a13226fae0d7a
kxade <ndibragim@gmail.com>
2024-09-30 22:51:33 +0000

    update store method in PostController api

--- app/Http/Controllers/API/V1/PostController.php
+++ app/Http/Controllers/API/V1/PostController.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Post\StorePostRequest;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
+use App\Enums\PostSource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -23,18 +25,26 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        $validated = $request->validate([
+        $fields = $request->validate([
             'title' => ['required', 'string', 'max:100'],
             'content' => ['required', 'string', 'max:1000'],
-            'published_at' => ['nullable', 'string', 'date'],
+            'published_at' => ['nullable', 'date'],
             'published' => ['nullable', 'boolean'],
-            'category_id' => 'nullable|exists:categories,id',
-            'user_id' => 'required|exists:users,id'
+            'category_id' => ['nullable', 'exists:categories,id'],
         ]);
 
-        $post = Post::create($validated);
+        if (empty($fields['published_at'])) {
+            $fields['published_at'] = now();
+        }
 
-        return new PostResource($post);
+        $fields['source'] = PostSource::Api;
+
+        $post = Auth::user()->posts()->create($fields);
+
+        return response()->json([
+            'message' => 'Post created successfully!',
+            'post' => $post
+        ], 201);
     }
 
     /**
