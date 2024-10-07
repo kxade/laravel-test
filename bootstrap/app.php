260a992547ced6e7307840b4e63a6a7b8659981b
kxade <ndibragim@gmail.com>
2024-10-07 22:36:37 +0000

    delete unnesessary files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,8 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
         then: function () {
             Route::middleware('web')
                 ->group(base_path('routes/user.php'));
-            Route::middleware('web')
-                ->group(base_path('routes/admin.php'));
         },
     )
     ->withMiddleware(function (Middleware $middleware) {
