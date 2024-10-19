1e04ac23be60428c2aee13700eef54e9f4a5d696
kxade <ndibragim@gmail.com>
2024-10-19 19:19:08 +0000

    fix header view

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -5,11 +5,13 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbar-collapse">
-        @php
-          function active_link(string $name, string $active = 'active'): string {
-              return Route::is($name) ? $active : '';
-          }
-        @endphp
+        @if (!function_exists('active_link'))
+          @php
+            function active_link(string $name, string $active = 'active'): string {
+                return Route::is($name) ? $active : '';
+            }
+          @endphp
+        @endif
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
             <a href="{{ route('home') }}" class="nav-link {{active_link('home') }}" aria-current="page" >
