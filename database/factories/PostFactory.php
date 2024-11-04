6ef5342cb440a759563dc4bf40750c4558528983
kxade <ndibragim@gmail.com>
2024-11-04 23:13:05 +0000

    add Post facrtory for tests

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Enums\PostSource;
+
+
+class PostFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraphs(3, true),
+            'published' => $this->faker->boolean(80), // 80% chance of being published
+            'source' => $this->faker->randomElement([PostSource::App, PostSource::Api]),
+            'published_at' => $this->faker->boolean ? $this->faker->dateTimeThisYear : null,
+            'category_id' => null,
+            'user_id' => User::factory(),
+            'tags' => $this->faker->words(3),
+        ];
+    }
+}
